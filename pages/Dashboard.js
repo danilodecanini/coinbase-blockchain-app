dd2227f5721f2117973ebceda97b27958fe9eb5a
Danilo de Canini <danilodecanini@gmail.com>
2022-01-31 20:13:26 -0300

    feat: add frontend components to style page

---
+++ pages/Dashboard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
+
+const Dashboard = ({ address }) => {
+  return (
+    <Wrapper>
+      <Sidebar />
+      <MainContainer>
+        <Header />
+        <Main />
+      </MainContainer>
+    </Wrapper>
+  )
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  overflow: hidden;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
